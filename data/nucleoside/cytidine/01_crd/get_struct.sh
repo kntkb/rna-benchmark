d0fb4eba230fe24ac493ddb18ff1af081ea95917
kt <kntkb.qbr@gmail.com>
2024-04-13 10:47:12 -0400

    add nucleoside

---
+++ data/nucleoside/cytidine/01_crd/get_struct.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep "  1  "  ../../../tetramer/cccc/01_crd/rna_noh.pdb > rna_noh.pdb
---
+++ data/nucleoside/uridine/01_crd/get_struct.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grep "  1  "  ../../../tetramer/uuuu/01_crd/rna_noh.pdb > rna_noh.pdb
